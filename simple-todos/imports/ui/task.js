862a86b57b5235acafc807a77a3e07cf76f78872
Hacksareal <knm.my@mail.ru>
2017-03-02 14:53:28 +0300

    third commit

--- simple-todos/imports/ui/task.js
+++ simple-todos/imports/ui/task.js
@@ -4,12 +4,21 @@ import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../api/tasks.js';
  
 import './task.html';
+
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
  
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
          Meteor.call('tasks.setChecked', this._id, !this.checked);
     
+  },
+    'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   },
   'click .delete'() {
     Meteor.call('tasks.setChecked', this._id, !this.checked);
